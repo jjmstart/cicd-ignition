40be45bcad6f68784d9ecdf649174fa7609e5346
jjm_start <jjm_start@outlook.com>
2026-03-26 08:15:09 +0800

    feat: implement test slot 10001 and dynamic git sha tag

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// 定义一个简单的路由，返回一段标识信息
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello CI/CD! The pipeline is working. Version: 1.0.0")
+		fmt.Fprintf(w, "Hello SRE! Dynamic Tag Injection Works on Port 10001! Version: 2.0.0")
 	})
 	
 	// 监听 8080 端口
